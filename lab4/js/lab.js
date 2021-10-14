b79e88d198ec6cdcc261999a86c51d859b805233
DeserveToLose <91556672+DeserveToLose@users.noreply.github.com>
2021-10-14 18:43:20 +0800

    lab4

---
+++ lab4/js/lab.js
@@ -0,0 +1,25 @@
+//lab4
+//Name: Cong Wang
+//Partner: Tiange Wei
+
+
+
+// Go Fishing!
+// Make sure all equipments are ready
+// Driving to my destinition
+// Start fishing
+// Fish biting! Fighting with it!
+// Got it!
+//
+// Play Tom Clancy’s Rainbow Six Siege
+// Turn on my PC
+// Login my Ubisoft account
+// Login game
+// Start ranking, searching for a match
+  // Match found--Map:Kafe Dostoyevsky
+    // Attack first, pick Twitch
+      // Search for bombs. HOLD X TO SCAN FOR ENEMIES.
+      // Locate the bomb location and defuse it
+// The bomb is defused, win the first round
+// Your PC ran into a problem that it counldn't (never) handle and now it needs to restart.
+// Turn off my PC, pick up my phone, hang the **** PC on eBay
