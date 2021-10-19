ae31a518344c2857e0cbfc407c5415efca54379d
DeserveToLose <91556672+DeserveToLose@users.noreply.github.com>
2021-10-19 18:59:03 +0800

    lab5

---
+++ lab5/js/lab.js
@@ -0,0 +1,26 @@
+/**
+ * Author:    Cong Wang
+ * Created:   10.18.2021
+ *
+ * (c) Copyright by Blub Corp.
+ **/
+
+
+ // Define Variables
+ make = "Lexus";
+ model = "Lc500";
+ color = "white";
+ year = "2020";
+
+ //calculate
+age = 2021 - year;
+
+//output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " year<br>")
+
+
+ 
