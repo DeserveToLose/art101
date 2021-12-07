4ab9c9d8145b17957d6ef4039784769ca5184aaf
DeserveToLose <91556672+DeserveToLose@users.noreply.github.com>
2021-12-07 20:11:20 +0800

    lab15

--- lab15/js/lab.js
+++ lab15/js/lab.js
@@ -6,9 +6,30 @@
  **/
 
 
- $("#activate").click(function(){
+ var myUrl = "https://cataas.com/cat?json=true";
 
+ $("#myButton").click(callAjax);
 
+  function callAjax(){
+    console.log("click");
+    $.ajax({
 
+      url: myUrl,
+      //data: {},
+      type: "GET",
+      dataType: "json",
 
- })
+      })
+
+    .done(function(data) {
+      console.log("Success:", data);
+      var newUrl = "https://cataas.com/" + data.url;
+      var newTag = "<img src= " + newUrl + ">";
+      $("#output").html(newTag);
+
+    })
+
+    .fail(function(request,error) {
+      console.log(request, error);
+    })
+  }
